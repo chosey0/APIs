7f1fc81fa03508b7c954b20bbb631604007e451c
Seokgyu Choi <csq1404@gmail.com>
2024-06-29 19:10:57 +0900

    binance

--- test.py
+++ test.py
@@ -1,4 +1,20 @@
-import os
+import os    
+import ccxt.pro as ccxtpro
+import asyncio
+import pprint
 
-from interface.base.messages_factory import BaseMessage
-print(BaseMessage.from_kwargs(tr_id="test").to_dict())
+async def main():
+    exchange = ccxtpro.binance(config={
+        'apiKey': os.getenv("BINANCE_RAPP"),
+        'secret': os.getenv("BINANCE_RSEC"),
+        'enableRateLimit': True,
+        'options': {
+            'defaultType': 'future'
+        }
+    })
+
+    while True:
+        ticker = await exchange.watch_trades(symbol="BTC/USDT")
+        pprint.pprint(ticker)
+
+asyncio.run(main())
